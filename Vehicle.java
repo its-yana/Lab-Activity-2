fc254c063d374c2d2b07e3290ce97c00781e6ebf
its-yana <yanaliquit2@gmail.com>
2026-09-25 01:47:33 -0400

    Refactor constructor

--- Vehicle.java
+++ Vehicle.java
@@ -1,18 +1,24 @@
 public class Vehicle {
-  
-  String brand;
-  String model;
-  int year;
-  
-  void displayInfo(){
-    System.out.println(brand + " - " + model + " - " + year);
-  }
-  
-  int calculateAge(){
-    return 2026 - year;
-  }
-  
-  boolean isVintage(){
-    return calculateAge() > 25;
-  }
+
+    String brand;
+    String model;
+    int year;
+
+    Vehicle(String brand, String model, int year) {
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+    }
+
+    void displayInfo(){
+        System.out.println(brand + " - " + model + " - " + year);
+    }
+
+    int calculateAge(){
+        return 2026 - year;
+    }
+
+    boolean isVintage(){
+        return calculateAge() > 25;
+    }
 }
